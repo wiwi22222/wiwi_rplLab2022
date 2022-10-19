cb23b75815e98413901c529ff45d6527bf96877e
wiwi22222 <wiwilestiani915@gmail.com>
2022-10-19 19:25:16 +0800

    commit

---
+++ DAY 1 CODING.py
@@ -0,0 +1,7 @@
+nama_depan = "wiwi"
+nama_belakang = "lestiani"
+umur = 19
+TB = 163.0
+BB = 51
+menikah = 'false'
+print('nama_lengkap :', nama_depan + nama_belakang)
---
+++ DAY 2 CODING.py
@@ -0,0 +1,14 @@
+nilai = int(input("masukan nilai :"))
+if nilai >= 85 :
+    print('A')
+if nilai >= 80 :
+    print('B')
+if nilai >= 75 :
+    print('C')
+if nilai >= 70 :
+    print('D')
+else :
+     print('E')
+print('nilai yang tidak termasuk :')
+
+#cek
